e8105921dc17731301606e8121cfda4699de9786
PettyCode <pettycode@git.com>
2024-05-21 14:55:46 -0700

    Finish lora script and add lora_bert for custom attention

--- lora.py
+++ lora.py
@@ -1,64 +1,37 @@
-### Adapted 3 imports:
-# Make sure you install peft
-from peft import get_peft_model, LoraConfig, TaskType
-### 
-import random, numpy as np, argparse
+import argparse
 from types import SimpleNamespace
-import csv
 
 import torch
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
-from sklearn.metrics import f1_score, accuracy_score
-### Switch to transformer models
-#from tokenizer import BertTokenizer
-#from bert import BertModel
-from transformers import BertModel, BertTokenizer
-###
+from torch.utils.data import  DataLoader
+
+from bert import BertModel
 from optimizer import AdamW
 from tqdm import tqdm
 
+### Added imports below, removed some from above
+from classifier import model_eval, save_model, load_data, test, seed_everything
+from classifier import SentimentDataset, BertSentimentClassifier
+
+from lora_bert import LoRABertSelfAttention, LoRALayer
+###
 
 TQDM_DISABLE=False
 
 
-# Fix the random seed.
-def seed_everything(seed=11711):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-
-
-# Adapt BertSentimentClassifier to use LoRA-Config
 class BertSentimentClassifier(torch.nn.Module):
-    def __init__(self, config):
+    '''
+    This module performs sentiment classification using BERT embeddings on the SST dataset.
+
+    In the SST dataset, there are 5 sentiment categories (from 0 - "negative" to 4 - "positive").
+    Thus, your forward() should return one logit for each of the 5 classes.
+    '''
+    def __init__(self, config, r):
         super(BertSentimentClassifier, self).__init__()
         self.num_labels = config.num_labels
-
-        ### NEW
-
-        # Load BERT configuration and set use_return_dict explicitly, otherwise bug
-        # only necessary with default model
-        # bert_config = BertModel.from_pretrained('bert-base-uncased').config
-        # bert_config.use_return_dict = True
         self.bert = BertModel.from_pretrained('bert-base-uncased')
-
-        # Apply LoRA configuration
-        lora_config = LoraConfig(
-            r=8,
-            lora_alpha=16,
-            target_modules=["query", "value"],
-            lora_dropout=0.1,
-            bias="none",
-            task_type=TaskType.SEQ_CLS  # sequence classification
-        )
-
-        self.bert = get_peft_model(self.bert, lora_config)
-
+        ### Apply LoRA self-attention layers to the BERT model.
+        self.bert = replace_attention_layers(self.bert, r)
         ###
 
         # Pretrain mode does not require updating BERT paramters.
@@ -73,184 +46,29 @@ class BertSentimentClassifier(torch.nn.Module):
         self.linear = torch.nn.Linear(config.hidden_size, config.num_labels)
         self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
 
+        return
+
+
     def forward(self, input_ids, attention_mask):
         '''Takes a batch of sentences and returns logits for sentiment classes'''
-        bert_out = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        pooler_out = bert_out['pooler_output']  # (N, D) (the embeddings of the [CLS] token, 'embeds[:, 0, :]')
+        # The final BERT contextualized embedding is the hidden state of [CLS] token (the first token).
+        # HINT: You should consider what is an appropriate return value given that
+        # the training loop currently uses F.cross_entropy as the loss function.
+        ### 
+
+        bert_out = self.bert(input_ids, attention_mask)
+        pooler_out = bert_out['pooler_output']   # (N, D)  (the embeddings of the [CLS] (first) token, 'embeds[:, 0, :]')
         out = self.linear(self.dropout(pooler_out))
 
         return out
 
-
-
-class SentimentDataset(Dataset):
-    def __init__(self, dataset, args):
-        self.dataset = dataset
-        self.p = args
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        return self.dataset[idx]
-
-    def pad_data(self, data):
-        sents = [x[0] for x in data]
-        labels = [x[1] for x in data]
-        sent_ids = [x[2] for x in data]
-
-        encoding = self.tokenizer(sents, return_tensors='pt', padding=True, truncation=True)
-        token_ids = torch.LongTensor(encoding['input_ids'])
-        attention_mask = torch.LongTensor(encoding['attention_mask'])
-        labels = torch.LongTensor(labels)
-
-        return token_ids, attention_mask, labels, sents, sent_ids
-
-    def collate_fn(self, all_data):
-        token_ids, attention_mask, labels, sents, sent_ids= self.pad_data(all_data)
-
-        batched_data = {
-                'token_ids': token_ids,
-                'attention_mask': attention_mask,
-                'labels': labels,
-                'sents': sents,
-                'sent_ids': sent_ids
-            }
-
-        return batched_data
-
-
-class SentimentTestDataset(Dataset):
-    def __init__(self, dataset, args):
-        self.dataset = dataset
-        self.p = args
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        return self.dataset[idx]
-
-    def pad_data(self, data):
-        sents = [x[0] for x in data]
-        sent_ids = [x[1] for x in data]
-
-        encoding = self.tokenizer(sents, return_tensors='pt', padding=True, truncation=True)
-        token_ids = torch.LongTensor(encoding['input_ids'])
-        attention_mask = torch.LongTensor(encoding['attention_mask'])
-
-        return token_ids, attention_mask, sents, sent_ids
-
-    def collate_fn(self, all_data):
-        token_ids, attention_mask, sents, sent_ids= self.pad_data(all_data)
-
-        batched_data = {
-                'token_ids': token_ids,
-                'attention_mask': attention_mask,
-                'sents': sents,
-                'sent_ids': sent_ids
-            }
-
-        return batched_data
-
-
-# Load the data: a list of (sentence, label).
-def load_data(filename, flag='train'):
-    num_labels = {}
-    data = []
-    if flag == 'test':
-        with open(filename, 'r') as fp:
-            for record in csv.DictReader(fp,delimiter = '\t'):
-                sent = record['sentence'].lower().strip()
-                sent_id = record['id'].lower().strip()
-                data.append((sent,sent_id))
-    else:
-        with open(filename, 'r') as fp:
-            for record in csv.DictReader(fp,delimiter = '\t'):
-                sent = record['sentence'].lower().strip()
-                sent_id = record['id'].lower().strip()
-                label = int(record['sentiment'].strip())
-                if label not in num_labels:
-                    num_labels[label] = len(num_labels)
-                data.append((sent, label,sent_id))
-        print(f"load {len(data)} data from {filename}")
-
-    if flag == 'train':
-        return data, len(num_labels)
-    else:
-        return data
-
-
-# Evaluate the model on dev examples.
-def model_eval(dataloader, model, device):
-    model.eval() # Switch to eval model, will turn off randomness like dropout.
-    y_true = []
-    y_pred = []
-    sents = []
-    sent_ids = []
-    for step, batch in enumerate(tqdm(dataloader, desc=f'eval', disable=TQDM_DISABLE)):
-        b_ids, b_mask, b_labels, b_sents, b_sent_ids = batch['token_ids'],batch['attention_mask'],  \
-                                                        batch['labels'], batch['sents'], batch['sent_ids']
-
-        b_ids = b_ids.to(device)
-        b_mask = b_mask.to(device)
-
-        logits = model(b_ids, b_mask)
-        logits = logits.detach().cpu().numpy()
-        preds = np.argmax(logits, axis=1).flatten()
-
-        b_labels = b_labels.flatten()
-        y_true.extend(b_labels)
-        y_pred.extend(preds)
-        sents.extend(b_sents)
-        sent_ids.extend(b_sent_ids)
-
-    f1 = f1_score(y_true, y_pred, average='macro')
-    acc = accuracy_score(y_true, y_pred)
-
-    return acc, f1, y_pred, y_true, sents, sent_ids
-
-
-# Evaluate the model on test examples.
-def model_test_eval(dataloader, model, device):
-    model.eval() # Switch to eval model, will turn off randomness like dropout.
-    y_pred = []
-    sents = []
-    sent_ids = []
-    for step, batch in enumerate(tqdm(dataloader, desc=f'eval', disable=TQDM_DISABLE)):
-        b_ids, b_mask, b_sents, b_sent_ids = batch['token_ids'],batch['attention_mask'],  \
-                                                         batch['sents'], batch['sent_ids']
-
-        b_ids = b_ids.to(device)
-        b_mask = b_mask.to(device)
-
-        logits = model(b_ids, b_mask)
-        logits = logits.detach().cpu().numpy()
-        preds = np.argmax(logits, axis=1).flatten()
-
-        y_pred.extend(preds)
-        sents.extend(b_sents)
-        sent_ids.extend(b_sent_ids)
-
-    return y_pred, sents, sent_ids
-
-
-def save_model(model, optimizer, args, config, filepath):
-    save_info = {
-        'model': model.state_dict(),
-        'optim': optimizer.state_dict(),
-        'args': args,
-        'model_config': config,
-        'system_rng': random.getstate(),
-        'numpy_rng': np.random.get_state(),
-        'torch_rng': torch.random.get_rng_state(),
-    }
-
-    torch.save(save_info, filepath)
-    print(f"save the model to {filepath}")
-
+### NEW FUNCTION BELOW    
+# Replace the self-attention layers in the BERT model with LoRA self-attention layers.
+def replace_attention_layers(bert_model, r):
+    for i, layer in enumerate(bert_model.bert_layers):
+        layer.self_attention = LoRABertSelfAttention(bert_model.config, r)
+    return bert_model
+###
 
 def train(args):
     device = torch.device('cpu')
@@ -279,8 +97,21 @@ def train(args):
               'fine_tune_mode': args.fine_tune_mode}
 
     config = SimpleNamespace(**config)
+    ### Adapted to include the LoRA hyperparameter r.
+    lora_r = args.lora_r
+    model = BertSentimentClassifier(config, lora_r)
 
-    model = BertSentimentClassifier(config)
+    # Freeze the original BERT weights
+    for param in model.bert.parameters():
+        param.requires_grad = False
+
+    # Ensure LoRA layers' parameters are trainable
+    for param in model.bert.modules():
+        if isinstance(param, LoRALayer):
+            param.lora_A.weight.requires_grad = True
+            param.lora_B.weight.requires_grad = True
+    ###
+
     model = model.to(device)
 
     lr = args.lr
@@ -322,45 +153,6 @@ def train(args):
         print(f"Epoch {epoch}: train loss :: {train_loss :.3f}, train acc :: {train_acc :.3f}, dev acc :: {dev_acc :.3f}")
 
 
-def test(args):
-    with torch.no_grad():
-        device = torch.device('cpu')
-        if args.use_gpu:
-            if torch.cuda.is_available():
-                device = torch.device('cuda') 
-            elif torch.backends.mps.is_available():
-                device = torch.device('mps')
-        saved = torch.load(args.filepath)
-        config = saved['model_config']
-        model = BertSentimentClassifier(config)
-        model.load_state_dict(saved['model'])
-        model = model.to(device)
-        print(f"load model from {args.filepath}")
-        
-        dev_data = load_data(args.dev, 'valid')
-        dev_dataset = SentimentDataset(dev_data, args)
-        dev_dataloader = DataLoader(dev_dataset, shuffle=False, batch_size=args.batch_size, collate_fn=dev_dataset.collate_fn)
-
-        test_data = load_data(args.test, 'test')
-        test_dataset = SentimentTestDataset(test_data, args)
-        test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=args.batch_size, collate_fn=test_dataset.collate_fn)
-        
-        dev_acc, dev_f1, dev_pred, dev_true, dev_sents, dev_sent_ids = model_eval(dev_dataloader, model, device)
-        print('DONE DEV')
-        test_pred, test_sents, test_sent_ids = model_test_eval(test_dataloader, model, device)
-        print('DONE Test')
-        with open(args.dev_out, "w+") as f:
-            print(f"dev acc :: {dev_acc :.3f}")
-            f.write(f"id \t Predicted_Sentiment \n")
-            for p, s in zip(dev_sent_ids,dev_pred ):
-                f.write(f"{p} , {s} \n")
-
-        with open(args.test_out, "w+") as f:
-            f.write(f"id \t Predicted_Sentiment \n")
-            for p, s  in zip(test_sent_ids,test_pred ):
-                f.write(f"{p} , {s} \n")
-
-
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=11711)
@@ -374,6 +166,9 @@ def get_args():
     parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
     parser.add_argument("--lr", type=float, help="learning rate, default lr for 'pretrain': 1e-3, 'finetune': 1e-5",
                         default=1e-3)
+    ### Added LoRA hyperparameter r argument
+    parser.add_argument("--lora_r", type=int, help="rank for LoRA matrices", default=2)
+    ###
 
     args = parser.parse_args()
     return args
@@ -385,7 +180,9 @@ if __name__ == "__main__":
 
     print('Training Sentiment Classifier on SST...')
     config = SimpleNamespace(
-        filepath='sst-classifier.pt',
+        ### Added LoRA to filenames
+        filepath='-LoRA-sst-classifier.pt',
+        ###
         lr=args.lr,
         use_gpu=args.use_gpu,
         epochs=args.epochs,
@@ -395,8 +192,13 @@ if __name__ == "__main__":
         dev='data/ids-sst-dev.csv',
         test='data/ids-sst-test-student.csv',
         fine_tune_mode=args.fine_tune_mode,
-        dev_out = 'predictions/' + args.fine_tune_mode + '-sst-dev-out.csv',
-        test_out = 'predictions/' + args.fine_tune_mode + '-sst-test-out.csv'
+        ### Added LoRA to filenames
+        dev_out = 'predictions/' + args.fine_tune_mode + '-LoRA-sst-dev-out.csv',
+        test_out = 'predictions/' + args.fine_tune_mode + '-LoRA-sst-test-out.csv',
+        ###
+        ### Pass loRA hyperparameter r to config
+        lora_r = args.lora_r
+        ###
     )
 
     train(config)
@@ -406,7 +208,9 @@ if __name__ == "__main__":
 
     print('Training Sentiment Classifier on cfimdb...')
     config = SimpleNamespace(
-        filepath='cfimdb-classifier.pt',
+        ### Added LoRA to filenames
+        filepath='LoRA-cfimdb-classifier.pt',
+        ###
         lr=args.lr,
         use_gpu=args.use_gpu,
         epochs=args.epochs,
@@ -416,13 +220,16 @@ if __name__ == "__main__":
         dev='data/ids-cfimdb-dev.csv',
         test='data/ids-cfimdb-test-student.csv',
         fine_tune_mode=args.fine_tune_mode,
-        dev_out = 'predictions/' + args.fine_tune_mode + '-cfimdb-dev-out.csv',
-        test_out = 'predictions/' + args.fine_tune_mode + '-cfimdb-test-out.csv'
+        ### Added LoRA to filenames
+        dev_out = 'predictions/' + args.fine_tune_mode + '-LoRA-cfimdb-dev-out.csv',
+        test_out = 'predictions/' + args.fine_tune_mode + '-LoRA-cfimdb-test-out.csv',
+        ###
+        ### Pass loRA hyperparameter r to config
+        lora_r = args.lora_r
+        ###
     )
 
     train(config)
 
     print('Evaluating on cfimdb...')
     test(config)
-
-
---
+++ lora_bert.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+from bert import BertSelfAttention
+from utils import *
+
+class LoRALayer(nn.Module):
+    def __init__(self, in_features, out_features, r):
+        super(LoRALayer, self).__init__()
+        self.r = r
+        if r > 0:
+            self.lora_A = nn.Linear(in_features, r, bias=False)
+            self.lora_B = nn.Linear(r, out_features, bias=False)
+        else:
+            self.lora_A = None
+            self.lora_B = None
+    
+    def svd_init(self, weights):
+        if self.r > 0:
+            U, S, V = torch.svd(weights)
+            U_r = U[:, :self.r]  # (out_features, r)
+            S_r = torch.diag(S[:self.r])  # (r, r)
+            V_r = V[:, :self.r].t()  # (r, in_features)
+            
+            self.lora_A.weight.data = V_r
+            self.lora_B.weight.data = U_r @ S_r 
+
+    def forward(self, x):
+        if self.r > 0:
+            return self.lora_B(self.lora_A(x))
+        else:
+            return 0
+
+class LoRABertSelfAttention(BertSelfAttention):
+    def __init__(self, config, r):
+        super().__init__(config)
+
+        self.query_lora = LoRALayer(config.hidden_size, self.all_head_size, r)
+        self.value_lora = LoRALayer(config.hidden_size, self.all_head_size, r)
+
+        self.query_lora.svd_init(self.query.weight.data)
+        self.value_lora.svd_init(self.value.weight.data)
+
+    def transform(self, x, linear_layer, lora_layer):
+        bs, seq_len = x.shape[:2]
+        proj = linear_layer(x) + lora_layer(x)
+        proj = proj.view(bs, seq_len, self.num_attention_heads, self.attention_head_size)
+        proj = proj.transpose(1, 2)
+        return proj
+
+    def forward(self, hidden_states, attention_mask):
+        key_layer = self.transform(hidden_states, self.key, lambda x: 0)
+        value_layer = self.transform(hidden_states, self.value, self.value_lora)
+        query_layer = self.transform(hidden_states, self.query, self.query_lora)
+        attn_value = self.attention(key_layer, query_layer, value_layer, attention_mask)
+        return attn_value

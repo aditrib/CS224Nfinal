56a9d269737540c0f32b670e0f54b4dc930ee7f1
PettyCode <pettycode@git.com>
2024-06-01 15:30:21 -0700

    implement dora

--- lora_bert.py
+++ lora_bert.py
@@ -1,34 +1,18 @@
 import torch.nn as nn
-from bert import BertSelfAttention
+import torch.nn.functional as F
 from utils import *
-
-class LoRALayer(nn.Module):
-    def __init__(self, in_features, out_features, r):
-        super(LoRALayer, self).__init__()
-        self.r = r
-        if r > 0:
-            self.lora_A = nn.Linear(in_features, r, bias=False)
-            self.lora_B = nn.Linear(r, out_features, bias=False)
-
-            nn.init.normal_(self.lora_A.weight, mean=0.0, std=0.02)
-            nn.init.zeros_(self.lora_B.weight)
-        else:
-            self.lora_A = None
-            self.lora_B = None
-
-    def forward(self, x):
-        if self.r > 0:
-            return self.lora_B(self.lora_A(x))
-        else:
-            return 0
         
 class LoRALinear(nn.Module):
-    def __init__(self, original_linear, r):
+    def __init__(self, original_linear, r, dora = False):
         super(LoRALinear, self).__init__()
         self.original_linear = original_linear  # Keep the original layer
+        self.dora = dora
+        self.magnitude = None
+
         in_features = original_linear.in_features
         out_features = original_linear.out_features
 
+        # Freeze the original parameters, they won't be updated during training
         for param in self.original_linear.parameters():
             param.requires_grad = False
 
@@ -37,6 +21,9 @@ class LoRALinear(nn.Module):
             self.lora_B = nn.Linear(r, out_features, bias=False)
             nn.init.normal_(self.lora_A.weight, mean=0.0, std=0.02)
             nn.init.zeros_(self.lora_B.weight)
+
+            if dora:
+                self.magnitude = nn.Parameter(torch.ones(1, out_features))
         else:
             self.lora_A = None
             self.lora_B = None
@@ -44,21 +31,30 @@ class LoRALinear(nn.Module):
     def forward(self, x):
         original_output = self.original_linear(x)
         if self.lora_A is not None and self.lora_B is not None:
-            lora_output = self.lora_B(self.lora_A(x))
-            return original_output + lora_output
+            lora_update = self.lora_B(self.lora_A(x))
+            if self.dora:
+                updated_weights = self.original_linear.weight + lora_update
+                norms = updated_weights.norm(p=2, dim=0, keepdim=True)
+                directional_component = updated_weights / norms
+                adjusted_weights = self.magnitude.unsqueeze(0) * directional_component
+
+                # Perform the final linear operation with adjusted weights
+                return F.linear(x, adjusted_weights, self.original_linear.bias)
+            else:
+                return original_output + lora_update
         else:
             return original_output
 
     
-def inject_lora(bert_model, mode, r):
+def inject_lora(bert_model, mode, r, dora = False):
     for layer in bert_model.bert_layers:
         # Transformer layers
-        layer.self_attention.query = LoRALinear(layer.self_attention.query, r)
-        layer.self_attention.key = LoRALinear(layer.self_attention.key, r)
-        layer.self_attention.value = LoRALinear(layer.self_attention.value, r)
+        layer.self_attention.query = LoRALinear(layer.self_attention.query, r, dora)
+        layer.self_attention.key = LoRALinear(layer.self_attention.key, r, dora)
+        layer.self_attention.value = LoRALinear(layer.self_attention.value, r, dora)
 
         if mode in ['all-lin', 'all-lin-only']:
             # Other linear layers
-            layer.interm_dense = LoRALinear(layer.interm_dense, r)
-            layer.out_dense = LoRALinear(layer.out_dense, r)
+            layer.interm_dense = LoRALinear(layer.interm_dense, r, dora)
+            layer.out_dense = LoRALinear(layer.out_dense, r, dora)
     return bert_model
